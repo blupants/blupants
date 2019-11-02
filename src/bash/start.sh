10574ff2aa5394ac100b80934fde5d92842be9b8
blupants <blupants.robot@gmail.com>
2019-11-02 11:52:00 -0500

    Changed motion to be loaded only on robots with a camera.

--- src/bash/start.sh
+++ src/bash/start.sh
@@ -6,19 +6,30 @@ echo "Robot init script..."
 echo "Waiting 30 seconds for network interface startup..."
 sleep 15
 
-motion &
-
-sleep 15
-
-/usr/bin/rc_test_leds
-
-echo "QR Code WiFi setup..."
-/usr/local/bin/qrcode_wifi
+mkdir -p /tmp/motion
 
 sleep 5
 
+if ls /dev/video* 1> /dev/null 2>&1; then
+	echo "Initializing video capture..."
+	/usr/bin/motion &
+
+	sleep 10
+
+	/usr/bin/rc_test_leds
+
+	echo "QR Code WiFi setup..."
+	/usr/local/bin/qrcode_wifi
+
+	sleep 5
+else
+	echo "Video device not found. Skipping video capture."
+fi
+
 echo "WiFi setup completed!"
 /usr/bin/rc_blink &
+
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 sleep 5
 
